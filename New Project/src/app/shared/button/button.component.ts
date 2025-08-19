da87bffc5607a1fd40e2a843e4ac1048edac8a8a
Raj-Singh08 <vimal38051@gmail.com>
2025-08-19 11:50:55 +0530

    Multiple content Projection using select

--- New Project/src/app/shared/button/button.component.ts
+++ New Project/src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'button[appButton]',  //Any button element in project that also has appButton Attribute should be controled by this comp.
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
+   
 }
