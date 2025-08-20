954d2de1393df39d7e8a3ccd1b3fda40bad7995c
Raj-Singh08 <vimal38051@gmail.com>
2025-08-20 13:42:19 +0530

    HostBinding and HostListener

--- New Project/src/app/shared/control/control.component.ts
+++ New Project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -9,9 +9,16 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,   //Disable style encapsulation, now the compoenent css will effect other componenet too
   //  if we apply it correctly like we are doing in this controlcss,Simply saying now it will work as global css
   host:{
-    class:'control'   //add class to all host element
+    class:'control',   //add class to all host element
+    '(click)': 'onClick1()'  //Event Binding
   }
 })
 export class ControlComponent {
+//  @HostBinding('class') className = 'control';
+// @HostListener('click')  onClick(){ console.log('clicked!');  }
   label = input.required<string>();
+
+  onClick1(){
+    console.log('clicked');  }
+
 }
