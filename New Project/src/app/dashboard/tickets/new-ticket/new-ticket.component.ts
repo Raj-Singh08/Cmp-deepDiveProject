52937ed50f5b0fe20c715a3f49e31893726ce551
Raj-Singh08 <vimal38051@gmail.com>
2025-08-28 15:01:21 +0530

    Getting hold of teplate variable by viewChild insted of passing it in action method(onSubmit)

--- New Project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ New Project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -12,11 +12,16 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
 
+  @ViewChild('form') form?:ElementRef<HTMLFormElement>;
+  
+
   onSubmit(title:string,ticketText:string){
     // const enteredTitle=titleElement.value;
     // console.log(enteredTitle);
     console.log(title);
     console.log(ticketText);
+    // form.reset();   //clear all data from form
+    this.form?.nativeElement.reset();
   }
 
 }
