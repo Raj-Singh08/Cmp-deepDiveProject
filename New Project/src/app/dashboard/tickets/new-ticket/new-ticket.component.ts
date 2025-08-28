f813e77929da089a883ee93f7ae9a3ad9be07b31
Raj-Singh08 <vimal38051@gmail.com>
2025-08-28 15:08:15 +0530

    using viewChild() insted if @ViewChild decorator

--- New Project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ New Project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -12,7 +12,9 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
 
-  @ViewChild('form') form?:ElementRef<HTMLFormElement>;
+  // @ViewChild('form') form?:ElementRef<HTMLFormElement>; //@ViewChildern can be used to select multiple elements
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');  //using signal by viewChild method
+
   
 
   onSubmit(title:string,ticketText:string){
@@ -21,7 +23,8 @@ export class NewTicketComponent {
     console.log(title);
     console.log(ticketText);
     // form.reset();   //clear all data from form
-    this.form?.nativeElement.reset();
+    // this.form?.nativeElement.reset();   //Used when using @ViewChild decorator not signal
+    this.form().nativeElement.reset();   //Used when using viewChild Signal
   }
 
 }
